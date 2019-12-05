cddb196fa1d516bf99354732392ed0b5fef63b28
-v <alexb.inf.4891@gmail.com>
2019-12-05 11:27:06 -0500

    Changed the theme, and generated the png

--- week_49/script.R
+++ week_49/script.R
@@ -1,5 +1,5 @@
 # -------------------------------------------------------------------------#
-#                    Philadelphia PArkin Violations                        #
+#                    Philadelphia Parkin Violations                        #
 # -------------------------------------------------------------------------#
 # Elaborated by: Alex Bajaña
 
@@ -9,9 +9,8 @@ library(tidytuesdayR)
 library(tidyverse)
 library(janitor)
 library(ggthemes)
+library(ggpubr)
 library(scales)
-
-theme_
 
 # Create the directory if it's ausent:
 
@@ -37,7 +36,7 @@ sum_tab <- vars %>%
         pull(.x) %>% 
         tabyl %>% 
         rename_at(1,function(x).x) %>% 
-        arrange(percent) %>% 
+        arrange(desc(percent)) %>% 
         mutate_at(.x,list(~factor(.) %>% 
                             fct_reorder(n) 
                           )
@@ -66,10 +65,12 @@ frecuencies <- list(
       geom_col(aes_string(x = vx,
                           y = "n"),
                colour = colors) +
+      scale_y_continuous(labels = number) +
       ggplot2::coord_flip()+
       labs(title = titles)+
-      theme_wsj() +
-      theme(axis.title.x = element_blank())
+      theme_economist() +
+      theme(axis.title.x = element_blank(),
+            axis.title.y = element_blank())
     
   })
 
@@ -81,20 +82,44 @@ top_10_v <- sum_tab[["violation_desc"]]$violation_desc
 
 # Tiles:
 
-philly$tickets %>% 
+tiles <- philly$tickets %>% 
   # filter(issuing_agency %in% top_10_a) %>% 
   group_by_at(vars) %>% 
   summarise(fine = sum(fine,na.rm=T)) %>% 
   filter(violation_desc %in% top_10_v) %>% 
-  ggplot()+
-  geom_tile(aes(x =issuing_agency,y = violation_desc,fill = fine )) +
+  ggplot(aes(x =issuing_agency,y = violation_desc,fill = fine ))+
+  geom_tile() +
+  geom_text(aes(label = dollar(fine)), colour = "white",size = 5.5)+
   scale_fill_continuous(labels = dollar) +
-  labs(x = "Parking violation",y = "Issuing agency",
-       title = "¿Where the money goes?",
-       fill = "Fine") + 
-  theme_wsj()
+  labs(x = "Issuing agency",y = "Parking violation",
+       title = "¿Where does the money goes?",
+       fill = "Total fine earned") + 
+  theme_economist()+
+  theme(legend.position = "right",
+        legend.direction = "vertical",
+        axis.title = element_text(),
+        axis.title.x = element_text(size=16),
+        axis.title.y = element_text(size=16)
+        )
 
 
+# Grouping plots:
+
+plot_1 <- ggarrange(frecuencies[[1]],frecuencies[[2]])
+
+plot_2 <- ggarrange(plot_1,tiles,nrow =2)
+
+final_plot <- annotate_figure(plot_2,
+                              bottom = text_grob(" Data source: Jess Streeter,  Philly Open Data \n Elaboration: Alex Bajaña @AlexBajaa5",
+                                                 hjust = 1, x = 1, face = "italic", size = 10))
+
+# Adding the tiles:
+
+png("week_49/joint_plot.png",res = 250,units = "in",width = 20,height = 8)
+
+final_plot
+
+dev.off()
 
     
 
