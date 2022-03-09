7a760b6acb30ad05f2a3b6c9e8ac0ebc4f6aabd4
monogramatico123 <alexb.inf.4891@gmail.com>
2022-03-09 15:20:27 -0500

    Semana 10 2022

--- 2022/week_10/script.R
+++ 2022/week_10/script.R
@@ -2,11 +2,49 @@
 #                            EU Student mobility                            #
 # ------------------------------------------------------------------------- #
 
-tuesdata <- tidytuesdayR::tt_load('2022-03-08')
 
-# erasmus <- readr::read_csv('https://github.com/rfordatascience/tidytuesday/blob/master/data/2022/2022-03-08/erasmus.csv')
+# Libraries ---------------------------------------------------------------
 
-readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-03-08/erasmus.csv',)
+library(tidyverse)
+library(patchwork)
+library(circlize)
+library(psData)
+
+# Reading data ------------------------------------------------------------
+
+erasmus <- tidytuesdayR::tt_load("2022-03-08")
+
+erasmus_data <- erasmus$erasmus
+
+View(erasmus_data)
+
+erasmus_data %>% 
+  group_by(academic_year) %>% 
+  summarise(total = sum(participants))
+
+summary <- erasmus_data %>% 
+  filter(activity_mob == "Transnational youth meetings",
+         academic_year == "2019-2020") %>% 
+  group_by(sending_country_code,receiving_country_code) %>% 
+  summarise(total = sum(participants,na.rm = TRUE) )
 
 
-erasmus <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-03-08/erasmus.csv')
+summary %>% 
+  select(sending_country_code,receiving_country_code) %>% 
+  map(unique) %>% 
+  map(length)
+
+
+wide_data <- summary %>% 
+  pivot_wider(names_from = receiving_country_code,values_from = total) %>% 
+  column_to_rownames(var = "sending_country_code")
+
+countrycode <- read_csv("2022/week_10/country_iso.txt")
+
+countrycode %>% 
+  select()
+
+
+chordDiagram(wide_data)  
+  
+  
